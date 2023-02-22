67b668dbafdf8405b2305f00c760e36e57dad9d7
antikun <121240743+antikun@users.noreply.github.com>
2023-02-22 23:33:40 +0200

    Add draw function

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ function makeRipple(e) {
 const buttons = document.querySelectorAll(".btn");
 buttons.forEach(button => {
     button.addEventListener("click", makeRipple)
-})
+});
 
 
 const pen = document.querySelector("#pen");
 const slider = document.querySelector("#grid-slider");
+const pad = document.querySelector(".pad");
 
 function makeDivs(num) {
-    const pad = document.querySelector(".pad")
     while (pad.firstChild) {
         pad.removeChild(pad.firstChild);
     }
@@ -38,17 +38,50 @@ function makeDivs(num) {
     for (let i = 0; i < gridNum; i++) {
         const div = document.createElement("div");
         div.style.width = div.style.height = `${((pad.clientWidth) / num)}px`;
-        div.classList.add("grid-cell")
+        div.classList.add("grid-cell");
         pad.append(div);
-        div.addEventListener("mousedown", () => {
-            div.style.backgroundColor = black;
-        })
     }
-
 }
 
 makeDivs(slider.value);
 
+window.addEventListener("resize", () => {
+    let divWidth = `${pad.clientWidth / slider.value}px`;
+    const padWidth = pad.clientWidth;
+    const temp = padWidth;
+    console.log(divWidth)
+    console.log(padWidth)
+    // pad.childNodes.forEach(div => { div.width = div.height = divWidth });
+});
+
 slider.addEventListener("input", () => {
     makeDivs(slider.value);
 });
+
+pad.onmousedown = function (e) {
+    e.preventDefault();
+    console.log(e.target)
+    const mainColor = document.querySelector("#main-color");
+    const draw = (e) => {
+        e.preventDefault();
+        if (pad.contains(e.target) && e.target != pad) {
+            e.target.style.backgroundColor = mainColor.value;
+        }
+    }
+    draw(e);
+
+    pad.addEventListener("mousemove", draw);
+    document.addEventListener("mouseup", () => {
+        pad.removeEventListener("mousemove", draw);
+    });
+}
+
+const toolsBtns = document.querySelectorAll(".tools-btns");
+toolsBtns.forEach(button => {
+    button.addEventListener("click", () => {
+        for (let btn of toolsBtns) {
+            btn.classList.remove("selected")
+        }
+        button.classList.add("selected");
+    });
+})
